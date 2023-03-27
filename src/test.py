162c82981bfd09b6c90c6a3d71b026db1840fa58
salveamicus <jwosborne@knox.edu>
2023-03-27 10:49:05 -0500

    just a quick reorg, nothing big

--- src/test.py
+++ src/test.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import sys
 
-img = cv.imread(cv.samples.findFile("starry_night.jpg"))
+img = cv.imread(cv.samples.findFile("../assets/starry_night.jpg"))
 
 if img is None:
     sys.exit("Could not read image.")
@@ -10,4 +10,4 @@ cv.imshow("Display window", img)
 k = cv.waitKey(0)
 
 if k == ord("s"):
-    cv.imwrite("starry_night.png", img)
+    cv.imwrite("../assets/starry_night.png", img)
